ee37fa3403e8c53948f5175830a7d230b32d5ba0
Terrence Lam <skyuplam@gmail.com>
2016-11-25 23:11:50 +0100

    Update Readme

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { applyRouterMiddleware, Router, browserHistory } from 'react-router';
+import { applyRouterMiddleware, Router, hashHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { useScroll } from 'react-router-scroll';
 import injectTapEventPlugin from 'react-tap-event-plugin';
@@ -15,18 +15,18 @@ import './index.css';
 injectTapEventPlugin();
 
 // Create redux store with history
-// this uses the singleton browserHistory provided by react-router
+// this uses the singleton hashHistory provided by react-router
 // Optionally, this could be changed to leverage a created history
-// e.g. `const browserHistory = useRouterHistory(createBrowserHistory)();`
+// e.g. `const hashHistory = useRouterHistory(createBrowserHistory)();`
 const initialState = {};
-const store = configureStore(initialState, browserHistory);
+const store = configureStore(initialState, hashHistory);
 
 
 // Sync history and store, as the react-router-redux reducer
 // is under the non-default key ("routing"), selectLocationState
 // must be provided for resolving how to retrieve the "route" in the state
 import { selectLocationState } from './containers/App/selectors';
-const history = syncHistoryWithStore(browserHistory, store, {
+const history = syncHistoryWithStore(hashHistory, store, {
   selectLocationState: selectLocationState(),
 });
 
